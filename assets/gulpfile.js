6c5e6c88cbfb6c670285ed0f83048bda22beda3e
joeyfigaro <joey@joeyfigaro>
2015-02-18 11:48:06 -0500

    testing auto-deploy

--- assets/gulpfile.js
+++ assets/gulpfile.js
@@ -1,26 +1,26 @@
-var gulp = require('gulp'),
-    browserSync = require('browser-sync'),
-    reload = browserSync.reload,
-    sass = require('gulp-sass'),
-    run = require('gulp-run'),
-    git = require('gulp-git');
+// var gulp = require('gulp'),
+//     browserSync = require('browser-sync'),
+//     reload = browserSync.reload,
+//     sass = require('gulp-sass'),
+//     run = require('gulp-run'),
+//     git = require('gulp-git');
 
-// browser-sync task for starting the server.
-gulp.task('browser-sync', function() {
-  var files = ['css/styles.css', '../*.php'];
-  browserSync.init(files, { notify: true });
-  run('browser-sync start --server --files "css/*.css"').exec();
-});
+// // browser-sync task for starting the server.
+// gulp.task('browser-sync', function() {
+//   var files = ['css/styles.css', '../*.php'];
+//   browserSync.init(files, { notify: true });
+//   run('browser-sync start --server --files "css/*.css"').exec();
+// });
 
-// sass
-gulp.task('sass', function () {
-  return gulp.src('sass/*.scss')
-    .pipe(sass())
-    .pipe(gulp.dest('css/'))
-    .pipe(reload({ stream:true }));
-});
+// // sass
+// gulp.task('sass', function () {
+//   return gulp.src('sass/*.scss')
+//     .pipe(sass())
+//     .pipe(gulp.dest('css/'))
+//     .pipe(reload({ stream:true }));
+// });
 
-// default task
-gulp.task('default', ['sass', 'browser-sync'], function () {
-  gulp.watch("sass/**/*.scss", ['sass']);
-});
+// // default task
+// gulp.task('default', ['sass', 'browser-sync'], function () {
+//   gulp.watch("sass/**/*.scss", ['sass']);
+// });
