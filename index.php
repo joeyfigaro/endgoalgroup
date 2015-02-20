8867ee65d8278435270d099f32aa97a8727510c0
joeyfigaro <joey@joeyfigaro>
2015-02-19 20:03:53 -0500

    ...

--- index.php
+++ index.php
@@ -1,11 +1,7 @@
 <?php get_header(); ?>
 
-<!-- BEGIN CONTENT
--------------------------------------------------------------- -->
-<div id="content">
-    <div class="container">
- 
-    </div> <!-- end .container -->
-</div> <!-- end #content -->
+<main>
+    <div class="container"></div>
+</main> <!-- end #content -->
 
 <?php get_footer(); ?>
